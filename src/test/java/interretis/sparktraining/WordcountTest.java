ceb77b11c0f360c11c3a31aa8fa30f31b6f62830
Marek Dudek <marek.dudek@gmail.com>
2015-04-26 21:50:08 +0200

    Test simplified

--- src/test/java/interretis/sparktraining/WordcountTest.java
+++ src/test/java/interretis/sparktraining/WordcountTest.java
@@ -7,7 +7,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.hamcrest.Matchers;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -20,34 +19,48 @@ import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class WordcountTest {
 
-    public static final Tuple2Comparator TUPLE_2_COMPARATOR = new Tuple2Comparator();
     /** System under test. */
     private Wordcount wordcount;
 
-    private static SparkConf config;
-    private static JavaSparkContext context;
+    private static final Tuple2Comparator TUPLE_2_COMPARATOR = new Tuple2Comparator();
+    private static List<String> LINES;
+    private static List<Tuple2<String, Integer>> TUPLES = asList(
+            new Tuple2<String, Integer>("one", 10),
+            new Tuple2<String, Integer>("two", 9),
+            new Tuple2<String, Integer>("three", 8),
+            new Tuple2<String, Integer>("four", 7),
+            new Tuple2<String, Integer>("five", 6),
+            new Tuple2<String, Integer>("six", 5),
+            new Tuple2<String, Integer>("seven", 4),
+            new Tuple2<String, Integer>("eight", 3),
+            new Tuple2<String, Integer>("nine", 2),
+            new Tuple2<String, Integer>("ten", 1)
+    );
+
+    private static SparkConf CONFIG;
+    private static JavaSparkContext CONTEXT;
 
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws IOException {
 
-        config = new SparkConf();
-        config.setMaster("local");
-        config.setAppName("Wordcount");
+        CONFIG = new SparkConf();
+        CONFIG.setMaster("local");
+        CONFIG.setAppName("Wordcount");
 
-        context = new JavaSparkContext(config);
+        CONTEXT = new JavaSparkContext(CONFIG);
+
+        LINES = Files.readLines(new File("src/main/resources/words.txt"), Charsets.UTF_8);
     }
 
     @AfterClass
     public static void tearDownClass() {
 
-        context.stop();
+        CONTEXT.stop();
     }
 
     @Before
@@ -66,7 +79,7 @@ public class WordcountTest {
         final Wordcount wordcount = new Wordcount("src/main/resources/words.txt", "target/hdfs/wordcount/output");
 
         // when
-        wordcount.runJob(context);
+        wordcount.runJob(CONTEXT);
 
         // then it doesn't break
     }
@@ -75,29 +88,14 @@ public class WordcountTest {
     public void second_test() throws IOException {
 
         // given
-        final List<String> lines = Files.readLines(new File("src/main/resources/words.txt"), Charsets.UTF_8);
-
-        final JavaRDD<String> input = context.parallelize(lines);
-        wordcount = new Wordcount(input);
+        final JavaRDD<String> input = CONTEXT.parallelize(LINES);
 
         // when
-        final JavaPairRDD<String, Integer> output = wordcount.runJob2(context);
+        wordcount = new Wordcount(input);
+        final JavaPairRDD<String, Integer> output = wordcount.runJob2(CONTEXT);
 
-        // then it doesn't break
-        final JavaPairRDD<String, Integer> expected = context.parallelizePairs(
-                asList(
-                        new Tuple2<String, Integer>("one", 10),
-                        new Tuple2<String, Integer>("two", 9),
-                        new Tuple2<String, Integer>("three", 8),
-                        new Tuple2<String, Integer>("four", 7),
-                        new Tuple2<String, Integer>("five", 6),
-                        new Tuple2<String, Integer>("six", 5),
-                        new Tuple2<String, Integer>("seven", 4),
-                        new Tuple2<String, Integer>("eight", 3),
-                        new Tuple2<String, Integer>("nine", 2),
-                        new Tuple2<String, Integer>("ten", 1)
-                )
-        );
+        // then
+        final JavaPairRDD<String, Integer> expected = CONTEXT.parallelizePairs(TUPLES);
 
         final List<Tuple2<String, Integer>> expectedList = expected.collect();
         final List<Tuple2<String, Integer>> outputList = output.collect();
@@ -107,7 +105,7 @@ public class WordcountTest {
         Collections.sort(expectedList, TUPLE_2_COMPARATOR);
         Collections.sort(outputList, TUPLE_2_COMPARATOR);
 
-        for (int i=0; i < expectedList.size(); i++) {
+        for (int i = 0; i < expectedList.size(); i++) {
 
             final Tuple2<String, Integer> expectedTuple = expectedList.get(i);
             final Tuple2<String, Integer> outputTuple = outputList.get(i);
