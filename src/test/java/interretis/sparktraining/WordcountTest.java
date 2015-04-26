e7e9890efac2d90836ad13dc31a2246710a825c5
Marek Dudek <marek.dudek@gmail.com>
2015-04-26 21:06:31 +0200

    Two tests sharing Spark context

--- src/test/java/interretis/sparktraining/WordcountTest.java
+++ src/test/java/interretis/sparktraining/WordcountTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,26 +15,45 @@ public class WordcountTest {
     /** System under test. */
     private Wordcount wordcount;
 
+    private static SparkConf config;
+    private static JavaSparkContext context;
+
+    @BeforeClass
+    public static void setUpClass() {
+
+        config = new SparkConf();
+        config.setMaster("local");
+        config.setAppName("Wordcount");
+
+        context = new JavaSparkContext(config);
+    }
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+
+        final File outputDir = new File("./target/hdfs/wordcount/output");
+        if (outputDir.exists()) {
+            FileUtils.deleteDirectory(outputDir);
+        }
     }
 
     @Test
     public void test() throws IOException {
 
         // given
-        final File outputDir = new File("./target/hdfs/wordcount/output");
-        if (outputDir.exists()) {
-            FileUtils.deleteDirectory(outputDir);
-        }
-
         final Wordcount wordcount = new Wordcount("src/main/resources/words.txt", "target/hdfs/wordcount/output");
 
-        final SparkConf config = new SparkConf();
-        config.setMaster("local");
-        config.setAppName("some application");
+        // when
+        wordcount.runJob(context);
 
-        final JavaSparkContext context = new JavaSparkContext(config);
+        // then it doesn't break
+    }
+
+    @Test
+    public void second_test() throws IOException {
+
+        // given
+        final Wordcount wordcount = new Wordcount("src/main/resources/words.txt", "target/hdfs/wordcount/output");
 
         // when
         wordcount.runJob(context);
